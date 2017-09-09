69aa652205c7503dd79ba69283b1f0cea38cebd7
Felix.lin <fly_81211@hotmail.com>
2017-09-10 00:00:53 +0800

    modify socket init url path.

--- js/from_index.js
+++ js/from_index.js
@@ -294,7 +294,7 @@ $(document).ready(function() {
 
     });
 
-    var socket = io('http://localhost:3000');
+    var socket = io();
     socket.on('connect', function() {
         console.log("connect");
     });
