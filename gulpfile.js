5c5dde9f5e003d1b7fb12545a867e05d879f552b
felix.lin <fly_81211@hotmail.com>
2017-09-20 00:21:52 +0800

    Added sprites.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp       = require('gulp'),
     concat     = require('gulp-concat'),
     minifyCSS  = require('gulp-minify-css'),
     uglify     = require('gulp-uglify'),
-    rename     = require("gulp-rename");
-
+    rename     = require("gulp-rename"),
+    spritesmith = require('gulp.spritesmith'),
+    imageResize = require('gulp-image-resize');
 gulp.task('concat', function() {
     return gulp.src('./vendor/*/css/*.css')
         .pipe(concat('all.css'))
@@ -43,7 +44,29 @@ gulp.task('webp-move',function(){
     return gulp.src('./img/*.webp')
     .pipe(rename({dirname: ''}))
     .pipe(gulp.dest('./build/img'));
-})
+});
 
+gulp.task('resize-icon', function resize () {
+return gulp.src('./img/flag/new/*.png')
+    .pipe(imageResize({width: 50, height: 50}))
+    .pipe(gulp.dest('./img/flag/new_resize/'));
+});
 
-gulp.task('default',['minify-css','fonts-move','uglify','webp-move']);
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('./img/flag/new_resize/*.png')
+  .pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css',
+    padding: 5
+  }));
+
+  return spriteData.pipe(gulp.dest('./vendor/build/css/'));
+});
+
+gulp.task('sprite-move',function(){
+    return gulp.src('./vendor/build/css/*.png')
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('default',['resize-icon','sprite','minify-css','fonts-move','uglify','webp-move','sprite-move']);
